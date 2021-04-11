dd10b439cb7fdbe6030b689d600e7b6277f5ba41
Douzanee <66843732+Douzanee@users.noreply.github.com>
2021-04-10 22:07:04 -0300

    Finalização do trabalho.
    Correção no loop que "não terminava".

--- Multi Thread Grilho/grilhothread/Cricket.java
+++ Multi Thread Grilho/grilhothread/Cricket.java
@@ -2,11 +2,9 @@ package grilhothread;
 
 import java.util.Random;
 
-public class Cricket{	
-	public int finishLine;
-	public Cricket(String name, int finishLine) {
+public class Cricket{
+	public Cricket(String name) {
 		this.cricketName = name;
-		this.finishLine = finishLine;	
 	}
 	
 	String cricketName;
@@ -14,9 +12,10 @@ public class Cricket{
 	Random rand = new Random();
 	int currentPosition = 0;
 	int jumpCount = 0;
+	boolean finalizou = false;
 	
 	public void Jump() {
-		movementDistance = rand.nextInt(50);
+		movementDistance = rand.nextInt(100);
 		currentPosition += movementDistance;
 		jumpCount += 1;
 		System.out.println("O " + cricketName + " pulou " + movementDistance + "cm e já percorreu : " + currentPosition + "cm");
--- Multi Thread Grilho/grilhothread/MainCorrida.java
+++ Multi Thread Grilho/grilhothread/MainCorrida.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class MainCorrida {
 
     static int cricketsThatFinished = 0;
+    static int finishLine = 200;
     
 	public static void main(String[] args) {
         int cricketCount = 0;
@@ -12,22 +13,23 @@ public class MainCorrida {
         System.out.println("Quantos grilos se juntarão a essa corrida? ");  
         cricketCount = scanner.nextInt();
         System.out.println("Grilos na corrida " + cricketCount);
+        System.out.println("Qual será a distância da corrida? ");
+        finishLine = scanner.nextInt();
 
         ThreadController threads[] = null;
         Cricket[] cricketMain = new Cricket[cricketCount];
-        int finishLine = 200;
         
         for(int i = 0; i < cricketCount; i++) {
-        	cricketMain[i] = new Cricket("Grilo_" + i, finishLine);
+        	cricketMain[i] = new Cricket("Grilo_" + (i+1));
         }
         
         
-        IniciarCorrida(cricketCount, threads, cricketMain, cricketsThatFinished);
+        IniciarCorrida(cricketCount, threads, cricketMain);
         
         
 	}
 	
-    public static void IniciarCorrida(int cricketsCount, ThreadController[] thread, Cricket[] cricket, int cricketsThatFinished) {
+    public static void IniciarCorrida(int cricketsCount, ThreadController[] thread, Cricket[] cricket) {
     	thread = new ThreadController[cricketsCount];
     	
     	for(int i = 0; i < cricketsCount; i++) 
@@ -35,28 +37,49 @@ public class MainCorrida {
     		thread[i] = new ThreadController(cricket[i]);
     		thread[i].start();
     	}
-
-    	while(cricketsThatFinished < cricketsCount) {
-    	Race(cricketsCount, thread, cricket);
-    	
-    	
+        for (int i = 0; i < thread.length; i++) 
+        {
+          try 
+          {
+            thread[i].join();
+          } 
+          catch (InterruptedException e ) 
+          { 	
+              e.printStackTrace();
+          }
+        }
+    	while(cricketsThatFinished <= cricketsCount) {
+    	Race(cricketsCount, thread, cricket);    	
     	}
     }
 
     public static void Race(int cricketsCount, ThreadController[] thread, Cricket[] cricket) {
-    	
-    		for(int i = 0; i < cricketsCount; i++) 
-        	{
-
-    			if(cricket[i].currentPosition >= cricket[i].finishLine) 
-    			{
-    				System.out.println("O " + cricket[i].cricketName +" alcançou a linha de chegada com "+ cricket[i].jumpCount + " pulos");
-    				cricketsThatFinished ++;
+    	for(int i = 0; i < cricketsCount; i++) 
+        {
+    		if(cricket[i].finalizou != true)
+    		{
+    			if(cricket[i].currentPosition >= finishLine) 
+    			{	
+    			cricketsThatFinished ++;
+    			System.out.println("O " + cricket[i].cricketName +" alcançou a linha de chegada com " + cricket[i].jumpCount + " pulos.");
+    			cricket[i].finalizou = true;
     			}
     			else 
     			{
     				thread[i].run();
         		}
-        	}
-    	}
+    		}
+        }
+            for (int i = 0; i < thread.length; i++) 
+            {
+              try 
+              {
+                thread[i].join();
+              } 
+              catch (InterruptedException e ) 
+              {
+                  e.printStackTrace();
+              }
+            }
+    }
 }
